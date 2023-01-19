1bbc75ed9bf0dbc5230a6f72159b33cc2879d2d6
abdel fatah achouri <fatehachouri@outlook.fr>
2023-01-19 14:01:01 +0100

    commit message

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Container } from 'react-bootstrap';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Store from './components/Store';
+import About from './components/About';
+import Navbar from './components/Navbar'
+import ShoppingCartProvider from './context/ShoppingCartContext';
+const App=()=> {
+  return (
+    
+    <ShoppingCartProvider>
+    < Navbar/>
+    <Container  className="mb-4">
+    <Routes>
+    <Route path='/' element={<Home />}/>
+    <Route path='/store' element={<Store />}/>
+    <Route path='/about' element={<About />}/>
+    </Routes>
+    </Container>
+    </ShoppingCartProvider>
+  );
+}
+
+export default App;
---
+++ src/components/StoreItem.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Button, Card } from 'react-bootstrap'
+import formatCurrency from './formatCurrency'
+import { useShoppingCart } from '../context/ShoppingCartContext'
+const StoreItem = ({id,price,name,imgUrl}) => {
+    
+    const{getItemsQuantity,increaseCartQuantity,decreaseCartQuantity,removeCartItem}=useShoppingCart()
+    const quantity= getItemsQuantity(id);
+  return (
+    <Card className='h-100'>
+        <Card.Img src={imgUrl} variant='top' 
+        style={{height:"200px",objectFit:"cover"}}/>
+        <Card.Body>
+            <Card.Title className='d-flex 
+            justify-content-between align-items-baseline'>
+                <span className='fs-2'>{name}</span>
+                <span className='text-muted ml-2'>
+                    {formatCurrency(price)}</span>
+            </Card.Title>
+            <div className='mt-auto'>
+                {quantity===0 ? 
+                (<Button className='w-100' onClick={()=>increaseCartQuantity(id)}> Add To Cart</Button>) : 
+                (<div className='d-flex align-items-center flex-column' 
+                style={{gap:"1.5rem"}}>
+                    <div 
+                    className='d-flex align-items-center justify-content-center '
+                    style={{gap:"1.5rem"}}>
+                        <Button onClick={()=>decreaseCartQuantity(id)}>-</Button>
+                        <span className='fs-3'>{quantity} in cart</span>
+                        <Button onClick={()=>increaseCartQuantity(id)}>+</Button>
+                    </div>
+                    <Button onClick={()=>removeCartItem(id)} variant='danger' size='sm' >
+                        Remove
+                        </Button>
+                </div>)}
+
+            </div>
+        </Card.Body>
+    </Card>
+  )
+}
+
+export default StoreItem
